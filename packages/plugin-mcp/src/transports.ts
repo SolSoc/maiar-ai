4f2c52b7b9acc1be55ff7ecf49239f8addb3c499
SolSoc <socrates.nft@gmail.com>
2025-05-03 19:53:59 -0700

    fix: follow guidelines for returns

--- packages/plugin-mcp/src/transports.ts
+++ packages/plugin-mcp/src/transports.ts
@@ -7,24 +7,18 @@ export function clientCommand(
   command?: string,
   serverScriptPath?: string
 ): string {
-  let result;
+  if (command) return command;
 
-  if (command) {
-    result = command;
-  } else if (serverScriptPath) {
+  if (serverScriptPath) {
     const isPython = serverScriptPath.endsWith(".py");
-    result = isPython
+    return isPython
       ? process.platform === "win32"
         ? "python"
         : "python3"
       : process.execPath;
-  } else {
-    throw new Error(
-      "MCP config needs either {command,args} or serverScriptPath"
-    );
   }
 
-  return result;
+  throw new Error("MCP config needs either {command,args} or serverScriptPath");
 }
 
 export function buildTransport(config: StdioConfig): StdioClientTransport;
@@ -34,32 +28,30 @@ export function buildTransport(
 export function buildTransport(
   config: ServerConfig
 ): StdioClientTransport | StreamableHTTPClientTransport {
-  let transport;
   if ("url" in config) {
     const url = new URL(config.url);
-    transport = new StreamableHTTPClientTransport(url);
-  } else {
-    const { serverScriptPath, command, args = [], env } = config as StdioConfig;
-
-    const cmd = clientCommand(command, serverScriptPath);
-
-    // put the script path as first argument
-    if (serverScriptPath) args.unshift(serverScriptPath);
-
-    const processEnv = Object.fromEntries(
-      Object.entries(process.env).filter(([, v]) => v !== undefined) as [
-        string,
-        string
-      ][]
-    );
-    transport = new StdioClientTransport({
-      command: cmd,
-      args,
-      env: {
-        ...processEnv,
-        ...(env || {})
-      }
-    });
+    return new StreamableHTTPClientTransport(url);
   }
-  return transport;
+
+  const { serverScriptPath, command, args = [], env } = config as StdioConfig;
+  const cmd = clientCommand(command, serverScriptPath);
+
+  // put the script path as first argument
+  if (serverScriptPath) args.unshift(serverScriptPath);
+
+  const processEnv = Object.fromEntries(
+    Object.entries(process.env).filter(([, v]) => v !== undefined) as [
+      string,
+      string
+    ][]
+  );
+
+  return new StdioClientTransport({
+    command: cmd,
+    args,
+    env: {
+      ...processEnv,
+      ...(env || {})
+    }
+  });
 }
